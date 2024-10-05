db5560f5d3e62f0de5b5376a8b08564698387103
kn134133 <46958161+kn134133@users.noreply.github.com>
2024-10-05 18:50:44 +0800

    Update branding.ts

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -2,9 +2,9 @@
 // if you want to use it in the commercial usage
 // please contact us for more information: hello@lobehub.com
 
-export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
+export const LOBE_CHAT_CLOUD = '智视法学 Cloud';
 
 export const BRANDING_NAME = '智视法学';
 export const BRANDING_LOGO_URL = '';
 
-export const ORG_NAME = 'LobeHub';
+export const ORG_NAME = '智视法学';
