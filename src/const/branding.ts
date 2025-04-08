c076f8ec20123c86c09a45f14a2a5c2d24a78c29
kn134133 <46958161+kn134133@users.noreply.github.com>
2025-04-08 11:18:39 +0800

    Update branding.ts

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -2,9 +2,9 @@
 // if you want to use it in the commercial usage
 // please contact us for more information: hello@lobehub.com
 
-export const LOBE_CHAT_CLOUD = '汉字溯源 Cloud';
+export const LOBE_CHAT_CLOUD = '基于大模型的爬虫分析系统 Cloud';
 
-export const BRANDING_NAME = '汉字溯源';
+export const BRANDING_NAME = '基于大模型的爬虫分析系统';
 export const BRANDING_LOGO_URL = '';
 
-export const ORG_NAME = '汉字溯源';
+export const ORG_NAME = '基于大模型的爬虫分析系统';
